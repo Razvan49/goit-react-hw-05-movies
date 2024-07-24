35a3cf30fa0c706d154dfc742d60fc433c46d86b
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2024-07-24 13:11:00 +0300

    First Try

---
+++ src/components/utils/utils.js
@@ -0,0 +1,5 @@
+const API_KEY = '24996ba1a77f5ecd2a2a683701ea072b';
+const getTrendMovieUrl = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=24996ba1a77f5ecd2a2a683701ea072b`;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export { API_KEY, BASE_URL, getTrendMovieUrl };
